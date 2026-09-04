2782ae0473973c9df4965b9f274aeaff21890c3b
aqila-mazaya <amazaya.ghaisani@gmail.com>
2026-09-04 07:48:08 +0700

    landing

--- src/pages/LandingPage.vue
+++ src/pages/LandingPage.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="landing">
      
-  <section class="hero bg-blue-150 p-[100px] flex-col items-center text-center text-white gap-[18px]">
+  <section class="hero bg-yellow-100 p-[100px] flex-col items-center text-center text-white gap-[18px]">
     <h1 class="text-[40px] text-center">hello welcome in sunfloownline shop</h1>
     <p class="text-[28pt] landing-[80pt]">mari mari menghamburkan uang</p>
-    <button class="mb-8 mt-[20px] text-[25px] bg-white text-olive-500 py-[40px] py-[10px] rounded-4xl transition duration-150 hover:bg-blue-200 active:scale-95">
+    <button class="mb-8 mt-[20px] text-[25px] bg-white text-olive-500 py-[20px] py-[10px] rounded-4xl transition duration-150 hover:bg-blue-200 active:scale-95">
       <router-link to="/product" class="hero-btn">yukss</router-link>
     </button>
     <center>
@@ -12,9 +12,9 @@
         <source src="https://www.w3schools.com/html/mov_bbb.mp4" type="video/mp4" />
       </video>
     </center>
-  </section>
-  </div>
-  <main>
+  </section> 
+</div>
+<main>
     <div class="ai">
       <h1>hello welcome in sunfloownline shop</h1>
       <p>mari mari menghamburkan uang</p>
